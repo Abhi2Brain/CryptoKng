01f1b397804314bb570bef96cdd89e987b2f7a27
Abhi2Brain <abhishek.20203@knit.ac.in>
2023-08-24 15:03:02 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
       textAlign={'center'}
       letterSpacing={["unset","widest"]}
       >
-Hello and greetings! I am Abhishek Kumar, currently in the concluding year of my studies at Kamla Nehru Institute of Technology in Sultanpur. I am delighted to present a project that I have diligently crafted using the ReactJS framework. Furthermore, I extend a cordial invitation for you to stay connected with me through my diverse online platforms, ensuring you are well-informed about forthcoming updates. Your keen interest and invaluable support hold immense significance to me. Thank you for your kind attention!
+Hello and greetings! I am Abhishek Kumar, currently in the final year of my studies at Kamla Nehru Institute of Technology in Sultanpur. I am delighted to present a project that I have diligently crafted using the ReactJS framework. Furthermore, I extend a cordial invitation for you to stay connected with me through my diverse online platforms, ensuring you are well-informed about forthcoming updates. Your keen interest and invaluable support hold immense significance to me. Thank you for your kind attention!
       </Text>
 
       <HStack marginTop={['9','14']} 
